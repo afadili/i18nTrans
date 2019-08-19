31c2c98700bdd29928ca39e01557591e57b3b2b8
root <afadili@gingalab.com>
2019-08-19 11:06:18 +0200

    test husky

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import BodyContent from './BodyContent'
-import './App.css';
-import i18n from './i18n';
-import { getI18n } from 'react-i18next';
-import { Trad } from './Trad'
-
+import React from "react";
+import BodyContent from "./BodyContent";
+import "./App.css";
+import i18n from "./i18n";
+import { getI18n } from "react-i18next";
+import { Trad } from "./Trad";
 
 class App extends React.Component {
   setLanguage(language) {
-    getI18n().changeLanguage(language)
+    getI18n().changeLanguage(language);
   }
-
-  render(){	
+  render() {
     return (
       <div className="App">
         <Trad text="title" />
-        <span>{ i18n.language}La langue est en français {
-          i18n.t("title")
-        } </span>
-        <select>{ ['en', 'pt'].map( language => 
-          <option onClick={() => this.setLanguage(language)}>{language}</option>
-        ) }</select>
-
+        <span>
+          {i18n.language}La langue est en français {i18n.t("title")}{" "}
+        </span>
+        <select>
+          {["en", "pt"].map(language => (
+            <option onClick={() => this.setLanguage(language)}>
+              {language}
+            </option>
+          ))}
+        </select>
         <BodyContent />
-      </div>   
-    )
-  }	  
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/BodyContent.js
+++ src/BodyContent.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import HeadContent from './HeadContent'
-import MainContent from './MainContent'
-import FootContent from './FootContent'
+import React from "react";
+import HeadContent from "./HeadContent";
+import MainContent from "./MainContent";
+import FootContent from "./FootContent";
 
 export default class BodyContent extends React.Component {
-  render(){
-    return <div>
-      <HeadContent />
-      <MainContent />
-      <FootContent />
-    </div>
+  render() {
+    return (
+      <div>
+        <HeadContent />
+        <MainContent />
+        <FootContent />
+      </div>
+    );
   }
 }
-
